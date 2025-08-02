b3e7a6175f307e369e3f589bbee17a9d5c6aa868
Sana Munir Alam <sanamuniralam25@gmail.com>
2025-08-02 01:15:26 -0400

    Commit

--- client/src/pages/CheckReservation.js
+++ client/src/pages/CheckReservation.js
@@ -49,7 +49,7 @@ const CheckReservation = () => {
     
     setIsLoading(true);
     try {
-      const response = await fetch(`http://localhost:3001/api/reservations/${reservationToCancel._id}`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/reservations/${reservationToCancel._id}`, {
         method: 'DELETE'
       });
       
@@ -102,7 +102,7 @@ const CheckReservation = () => {
         ? `email=${trimmedValue}` 
         : `phone=${trimmedValue.replace(/\D/g, '')}`;
 
-      const response = await fetch(`http://localhost:3001/api/reservations?${query}`);
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/reservations?${query}`);
       const data = await response.json();
 
       if (data.length === 0) {
--- client/src/pages/ReservationPage.js
+++ client/src/pages/ReservationPage.js
@@ -108,7 +108,7 @@ const Reservation = () => {
     if (validateStep()) {
       setIsLoading(true);
       try {
-        const response = await fetch('http://localhost:3001/api/reservations', {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/reservations`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({

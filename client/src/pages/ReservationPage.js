87abb5b3e9c4ce94561f256ce8bd28b38f9eb12f
Sana Munir Alam <sanamuniralam25@gmail.com>
2025-08-01 23:17:23 -0400

    Fixes

--- client/src/pages/ReservationPage.js
+++ client/src/pages/ReservationPage.js
@@ -108,7 +108,7 @@ const Reservation = () => {
     if (validateStep()) {
       setIsLoading(true);
       try {
-        const response = await fetch('https://little-lemon-backend-j1dg.onrender.com/api/reservations', {
+        const response = await fetch('${process.env.REACT_APP_API_URL}/api/reservations', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
